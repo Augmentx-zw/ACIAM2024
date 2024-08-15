d14d15275552150b333b45503cf2609d69428687
vincentsimonmarufu <60975980+vincentsimonmarufu@users.noreply.github.com>
2024-08-15 14:14:56 +0200

    finshed client side views

--- ACIAM/Controllers/HomeController.cs
+++ ACIAM/Controllers/HomeController.cs
@@ -4,29 +4,94 @@ using System.Diagnostics;
 
 namespace ACIAM.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult OrganisingCommittee()
+		{
+			return View();
+		}
+
+		public IActionResult LocalOrganisingCommittee()
+		{
+			return View();
+		}
+
+		public IActionResult ScientificCommittee()
+		{
+			return View();
+		}
+
+		public IActionResult HonoraryCommittee()
+		{
+			return View();
+		}
+
+		public IActionResult Speakers()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult Partners()
+		{
+			return View();
+		}
+
+		public IActionResult OnlineRegister()
+		{
+			return View();
+		}
+
+		public IActionResult PlenarySpeakers()
+		{
+			return View(); // Returns the PlenarySpeakers.cshtml view
+		}
+
+		public IActionResult InvitedSpeakers()
+		{
+			return View(); // Returns the InvitedSpeakers.cshtml view
+		}
+
+		public IActionResult SpecialSpeakers()
+		{
+			return View(); // Returns the SpecialSpeakers.cshtml view
+		}
+
+		public IActionResult HonorarySpeakers()
+		{
+			return View(); // Returns the HonorarySpeakers.cshtml view
+		}
+
+		public IActionResult SpeakerSingle()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
